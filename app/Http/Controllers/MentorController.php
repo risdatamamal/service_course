3cb8013f07ed9c5b366abc81d5d29d3fd0dad4af
Risda Tamam <risda123tamam@gmail.com>
2021-08-05 06:47:30 +0700

    Create API Mentor

---
+++ app/Http/Controllers/MentorController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mentor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class MentorController extends Controller
+{
+    public function create(Request $request)
+    {
+        $rules = [
+            'name' => 'required|string',
+            'profile' => 'required|url',
+            'profession' => 'required|string',
+            'email' => 'required|email'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        if($validator->failS())
+        {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $mentor = Mentor::create($data);
+
+        return response()->json(['status' => 'success', 'data' => $mentor]);
+    }
+}
